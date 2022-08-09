157481328f28796b75631ad85a14028b06241807
Natalie <nataliejhong@gmail.com>
2022-08-08 22:57:36 -0500

    first commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try{
+        //! Additional properties no longer needed
+        const conn = await mongoose.connect
+        (process.env.MONGO_URI)
+
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
